ce6a22a3c64212a970bb9065cc3054485299da30
KunjBhesaniya10 <kcbhesaniya10@gmail.com>
2026-04-30 19:32:43 +0530

    Add more testcases

--- test/drop.sql
+++ test/drop.sql
@@ -3,3 +3,4 @@ drop table gsi_index_file_state cascade;
 drop table gsi_customers_age cascade;
 drop table gsi_registry cascade;
 drop table gsi_transactions_customer_id cascade;
+drop table gsi_transactions_order_id cascade;

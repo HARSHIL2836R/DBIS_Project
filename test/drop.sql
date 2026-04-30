6b3cbe805667b366a847a99c0ed7db309af935e3
dhruvraj <dhruvrajmerchant.123@gmail.com>
2026-04-30 18:15:05 +0530

    drop.sql

---
+++ test/drop.sql
@@ -0,0 +1,5 @@
+drop table gsi_file_catalog cascade;
+drop table gsi_index_file_state cascade;
+drop table gsi_customers_age cascade;
+drop table gsi_registry cascade;
+drop table gsi_transactions_customer_id cascade;
